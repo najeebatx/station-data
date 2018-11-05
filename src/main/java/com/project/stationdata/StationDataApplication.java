c55453f1c7fafde9ce49d116924a81d35236cd8e
Najeeb Rahman <najeeb2000@gmail.com>
2018-11-04 19:23:49 -0600

    Initial commit

---
+++ src/main/java/com/project/stationdata/StationDataApplication.java
@@ -0,0 +1,12 @@
+package com.project.stationdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StationDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StationDataApplication.class, args);
+	}
+}
